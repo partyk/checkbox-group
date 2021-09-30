208dbb4320ae45e092618332dbe58aee54a5835e
Bohdan Partyk <info@podebrady.org>
2021-09-30 09:42:38 +0200

    wrapper class extend

--- src/CheckboxGroup.js
+++ src/CheckboxGroup.js
@@ -8,16 +8,19 @@ export default class CheckboxGroup {
         };
         this.init();
     }
+
     init() {
         document.addEventListener('change', (e) => {
             this.handleChange(e);
         });
     }
+
     handleChange(e) {
         if (e.target.classList.contains(this.options.class)) {
             this.change(e.target);
         }
     }
+
     change(input) {
         const groupId = input.getAttribute('data-group-id');
         const groupName = input.getAttribute('data-group-name');
@@ -28,27 +31,41 @@ export default class CheckboxGroup {
             this.checkGroup(groupName);
         }
     }
+
     selectAll(groupName, status) {
         document.querySelectorAll('input[data-group-name="' + groupName + '"]').forEach((e) => {
             this.changeStatus(e, status);
         });
         this.triggerChange(document.querySelector('input[data-group-id="' + groupName + '"]'));
     }
+
+    changeWrapperClass(input) {
+        if (!input.closest(this.options.wrapperSelector)) {
+            return;
+        }
+        if (input.checked) {
+            input.closest(this.options.wrapperSelector).classList.add(this.options.wrapperClass);
+        } else {
+            input.closest(this.options.wrapperSelector).classList.remove(this.options.wrapperClass);
+        }
+    }
+
     changeStatus(input, status) {
         input.checked = status;
         this.triggerChange(input);
     }
+
     checkGroup(groupName) {
         const elementId = document.querySelector('input[data-group-id="' + groupName + '"]');
         const elements = document.querySelectorAll('input[data-group-name="' + groupName + '"]');
         const elementsChecked = document.querySelectorAll('input:checked[data-group-name="' + groupName + '"]');
         this.changeStatus(elementId, elements.length === elementsChecked.length);
     }
+
     triggerChange(input) {
         // without explorer
         // const event = new Event('change');
         // input.dispatchEvent(event);
-
         const event = document.createEvent('Event');
         event.initEvent('checkBoxGroup:change', true, true); // can bubble, and is cancellable
         input.dispatchEvent(event);
